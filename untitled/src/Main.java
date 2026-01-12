9cba1005f84b8a96187d9611cd9124f518bdadc5
Sandip Sawrikar <sandipsawrikar1@gmail.com>
2026-01-12 09:57:16 +0530

    Leetcode  : Given an array of integers nums and an integer target return indices of the two numbers such that they add up to target.

---
+++ untitled/src/Main.java
@@ -0,0 +1,27 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 3, 5, 7};
+        int target = 8;
+        int[] result = twoSum(nums, target);
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> index = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (index.containsKey(target - num)) {
+                return new int[] { i, index.get(target - num) };
+            }
+            index.put(num, i);
+        }
+
+        return null;
+    }
+}
