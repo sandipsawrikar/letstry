75e2b227a8a4e144e47cb585104ec55bb5777df4
Sandip Sawrikar <sandipsawrikar1@gmail.com>
2026-04-14 15:16:56 +0530

    First jasper report

---
+++ JasperReport/jasperreport/src/main/java/com/sandip/JaspersoftApplication.java
@@ -0,0 +1,13 @@
+package com.sandip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaspersoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaspersoftApplication.class, args);
+    }
+
+}
---
+++ JasperReport/jasperreport/src/test/java/com/sandip/jasperreport/JasperreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandip.jasperreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperreportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
